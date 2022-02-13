05a99098c4814aa1257e2efa02f969db3a47eb5b
JamesHendryNZ <jameshendry@outlook.co.nz>
2022-02-13 23:49:51 +1300

    Add files via upload

--- the hihihi/weltecTimeTables.py
+++ the hihihi/weltecTimeTables.py
@@ -11,8 +11,8 @@ driver.get("http://timetable.weltec.ac.nz/2018")
 elem = driver.find_element_by_id("LinkBtn_locations").click()
 elem = driver.find_element_by_id("dlObject")
 
-driver.get_screenshot_as_file( '/tmp/b107ThisWeekSelectioned.png' )
-driver.save_screenshot('/tmp/b107TimeTableSelect.png')
+driver.getScreenShot_as_file( '/tmp/b107ThisWeekSelectioned' )
+driver.save_screenShot('/tmp/b107TimeTable');
 
 elem = driver.find_element_by_xpath('//*[@id="dlObject"]/option[47]')
 elem.click()
@@ -23,13 +23,12 @@ elem.click()
 elem = driver.find_element_by_xpath('//*[@id="lbWeeks"]/option[1]')
 elem.click()
 
-driver.get_screenshot_as_file( '/tmp/b107ThisWeekSelectioned.png' )
-driver.save_screenshot('/tmp/b107TimeTable.png');
+driver.getScreenShot_as_file( '/tmp/b107ThisWeekSelectioned' )
+driver.save_screenShot('/tmp/b107TimeTable');
 
 elem = driver.find_element_by_id("bGetTimetable")
 elem.click()
 
-driver.implicitly_wait( 5 )
-driver.get_screenshot_as_file( '/tmp/b107TimeTable.png' )
-driver.save_screenshot('/tmp/b107TimeTable.png')
-
+driver.implicity_wait( 5 )
+driver.getScreenShot_as_file( '/tmp/b107TimeTable' )
+driver.save_screenShot('/tmp/b107TimeTable');
--- the hihihi/zelda-Dungeon-drop-downMenus.py
+++ the hihihi/zelda-Dungeon-drop-downMenus.py
@@ -19,43 +19,29 @@ from pandas import ExcelFile
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 
-expectedResults = []
-
-loading the excel file with 
+#loading the excel file with 
 ecxelFilePath = 'testData.xlsx'
 xlFile = pd.read_excel( excelFilePath , sheetname = 'sheet1' )
-for i in xlFile:
-    expectedResults.append( xlFile['Expected Results'][i])
-
-#expectedResults = xlFile['Expected Headings']
+expectedResults = xlFile['Expected Headings']
 
 
 driver = webdriver.Firefox()
 driver.get("https://zeldadungeon.net")
-driver.get_screenshot_as_file( '/tmp/zeldaDungeonHomePage.png' )
-driver.save_screenshot('/tmp/zeldaDungeonHomePage.png');
+driver.getScreenShot_as_file( '/tmp/zeldaDungeonHomePage' )
+driver.save_screenShot('/tmp/zeldaDungeonHomePage');
 
-try:
-    hoverElem = WebDriverWait(driver , 10).until(EC.presence_of_element_located((By.XPATH , '//*[@id="sitenav-games"]/nav/ul/li[3]/a')))
-    #hoverElem = driver.find_element_by_xpath('//*[@id="sitenav-games"]/nav/ul/li[3]/a')    
-    hover = ActionChains(driver).move_to_element(hoverElem)
-    hover.perform()
-    elem = WebDriverWait(driver , 10).until( EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div[2]/nav/ul/li[3]/div/div[2]/ul/li[5]/a")))
-    elem = driver.find_element_by_xpath('//*[@id="sitenav-games"]/nav/ul/li[3]/div/div[2]/ul/li[5]/a')
-    elem.click()
-finally:
-    print('yo') #tempary will be change later
-#elem = driver.find_element_by_visibilty
+hoverElem = driver.find_element_by_xpath('//*[@id="sitenav-games"]/nav/ul/li[3]/a')
+hover = ActionChains(driver).move_to_element(hoverElem)
+hover.perform()
+elem = WebDriverWait( driver,2 ).until( EC.presence_of_element_located((By.PARTIAL_LINK, "Tower of Hera")))
+
+elem = driver.find_element_by_xpath('//*[@id="sitenav-games"]/nav/ul/li[3]/div/div[2]/ul/li[5]/a')
+elem.click()
+
+elem = driver.find_element_by_visibilty
 #this is where expected results from a spreed sheet will be
-#compared to those in the spred sheet
-for i in expectedResults:
-    
-    assert 
-
+#loaded and compared
 
 
 
